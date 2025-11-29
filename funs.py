d50def04eb4c8b51426ca79ee9e509b639af82c8
rahul prajapat <rahulp748977@gmail.com>
2025-11-29 16:37:28 +0530

    second function version

--- funs.py
+++ funs.py
@@ -6,14 +6,3 @@ def calculate(a, b):
 x, y = calculate(10, 5)
 print("Sum:", x)
 print("Difference:", y)
-
-
-def user_details(**info):
-    for key, value in info.items():
-        print(key, ":", value)
-
-user_details(name="Rahul", role="DevOps Engineer", experience=2)
-
-
-
-
